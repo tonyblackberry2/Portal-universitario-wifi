9f6dac5f3193e5ba3eb0275cf34947af0bb13a89
tonyblackberry2 <tonyblackberry2@gmail.com>
2025-04-22 13:55:40 -0300

    pqp

--- public/firebase.js
+++ public/firebase.js
@@ -1,26 +1,56 @@
 // Importe o SDK do Firebase
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    signOut 
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDocs,
+    query,
+    where,
+    orderBy,
+    limit 
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+import dotenv from 'dotenv';
+
+// Carrega as variáveis de ambiente
+dotenv.config();
 
 // Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBUcqTVfqHZIHc3MShV2UzCRKanI7EWSGE",
-  authDomain: "portal-universitario-wi-fi.firebaseapp.com",
-  projectId: "portal-universitario-wi-fi",
-  storageBucket: "portal-universitario-wi-fi.appspot.com",
-  messagingSenderId: "23877160613",
-  appId: "1:23877160613:web:022cac1e5024e7ec1c074c",
-  measurementId: "G-NDY1LQ18VC"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
-// Inicializar o Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Exportar variáveis e funções necessárias
-export { auth, db, getDocs, collection, addDoc, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+// Exporta as funções e variáveis necessárias
+export {
+    auth,
+    db,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    where,
+    orderBy,
+    limit
+};
 
 // Função de Login
 document.getElementById('loginFormElement').addEventListener('submit', async (e) => {
